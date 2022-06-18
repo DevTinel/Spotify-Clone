499373a290d4e039b32614570924f99384c82fd0
DevTinel <davidaugustolol36@gmail.com>
2022-06-18 17:27:13 -0300

    updt

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const button = document.querySelector('[data-button]')
 const menu = document.querySelector('.mobile')
-
+const body = document.querySelector('body')
 function ativar(e){
     e.preventDefault()
     menu.classList.toggle('ativo')
     button.classList.toggle('rotate')
 }
 
+
 button.addEventListener('click',ativar)
+
